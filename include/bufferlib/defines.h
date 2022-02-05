ecc3005026bfeb8bf75d03f95a4df30165b66637
Ravi Prakash Singh <rp0412204@gmail.com>
2022-02-05 18:01:54 +0530

    Updated defines.h

--- include/bufferlib/defines.h
+++ include/bufferlib/defines.h
@@ -1,8 +1,9 @@
-
-#ifdef BUILD_DYNAMIC_LIBRARY
-#	define BUF_API __declspec(dllexport)
+#ifdef BUF_STATIC_LIBRARY
+#	define BUF_API
 #elif BUF_DYNAMIC_LIBRARY
 #	define BUF_API __declspec(dllimport)
+#elif BUILD_DYNAMIC_LIBRARY
+#	define BUF_API __declspec(dllexport)
 #else
 #	define BUF_API
 #endif
